3ccc6015d85dd211148b46afac627b004b7e0e0c
Juho Saarelainen <bradurdung@gmail.com>
2020-02-22 01:58:57 +0200

    Generator

---
+++ Game/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using System.Linq;
+
+[System.Serializable]
+public class RoomType {
+    [MinValue(3), MaxValue(10)]
+    public int Width = 3;
+
+    [MinValue(3), MaxValue(10)]
+    public int Height = 3;
+}
+
+public class MapGenerator : MonoBehaviour
+{
+
+    [MinValue(50)]
+    [SerializeField]
+    private int worldWidth = 2000;
+    [MinValue(50)]
+    [SerializeField]
+    private int worldHeight = 2000;
+
+    [SerializeField]
+    private int numberOfRounds = 200;
+
+    [SerializeField]
+    private List<RoomType> roomTypes;
+
+    private SpriteRenderer spritePrefab;
+    private MazeCarver mazeCarverPrefab;
+
+
+    private List<Rect> rooms;
+    private Rect world;
+    void Start()
+    {
+        world = new Rect(0, 0, worldWidth, worldHeight);
+        CreateRoomSprite(world, Color.gray);
+        rooms = new List<Rect>();
+        for(int index = 0; index <= numberOfRounds; index += 1) {
+            Rect room = GenerateRoom();
+            bool overlaps = rooms.Any(existingRoom => room.Overlaps(existingRoom));
+            if (!overlaps) {
+                Rect minRoom = new Rect(room.x + 1, room.y + 1, room.width - 2, room.height - 2);
+                rooms.Add(room);
+                CreateRoomSprite(room, Color.green);
+                CreateRoomSprite(minRoom, Color.blue);
+            }
+        }
+        Carve();
+    }
+
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Created rect");
+            rooms.Add(GenerateRoom());
+        }*/
+    }
+
+    private void Carve() {
+        if (mazeCarverPrefab == null) {
+            mazeCarverPrefab = Resources.Load<MazeCarver>("MazeCarver");
+        }
+        MazeCarver mazeCarver = Instantiate(mazeCarverPrefab, Vector2.zero, Quaternion.identity);
+        mazeCarver.Initialize(world);
+    }
+
+    private SpriteRenderer CreateRoomSprite(Rect rect, Color color) {
+        if (spritePrefab == null) {
+            spritePrefab = Resources.Load<SpriteRenderer>("RoomSprite");
+        }
+        SpriteRenderer spriteRenderer = Instantiate(spritePrefab, Vector2.zero, Quaternion.identity);
+        spriteRenderer.transform.parent = transform;
+        spriteRenderer.transform.localScale = new Vector2(rect.width, rect.height);
+        spriteRenderer.transform.position = rect.position;
+        spriteRenderer.gameObject.SetActive(true);
+        spriteRenderer.color = color;
+        return spriteRenderer;
+    }
+
+    private Rect GenerateRoom() {
+        RoomType roomType = roomTypes[Random.Range(0, roomTypes.Count)];
+        int x = Random.Range((int)world.xMin, (int)world.xMax);
+        int y = Random.Range((int)world.yMin, (int)world.yMax);
+        /*Debug.Log(string.Format("{0}, {1}, {2}, {3}, {4}, {5}",
+            world.xMin,
+            world.xMax,
+            world.yMin,
+            world.yMax,
+            x,
+            y
+        ));*/
+
+        Rect room = new Rect(x, y, roomType.Width, roomType.Height);
+
+        return room;
+    }
+}
---
+++ Game/Assets/Scripts/MazeCarver.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MazeNode
+{
+    public MazeNode(int x, int y)
+    {
+        Rect = new Rect(x, y, 1, 1);
+    }
+
+    public Rect Rect;
+
+    public bool IsOpen = false;
+
+    public bool IsWall = false;
+
+    public override bool Equals(System.Object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else
+        {
+            MazeNode node = (MazeNode)obj;
+            return node.Rect == this.Rect;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return ((int)Rect.x << 2) ^ (int)Rect.y;
+    }
+
+
+}
+
+public class MazeCarver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private MazeNode[][] nodes;
+
+    private List<MazeNode> carvedNodes = new List<MazeNode>();
+
+    private List<Vector2> positions = new List<Vector2>()
+    {
+        new Vector2(0, -2),
+        new Vector2(2, 0),
+        new Vector2(0, 2),
+        new Vector2(-2, 0)
+    };
+
+    private SpriteRenderer spritePrefab;
+    public void Initialize(Rect world)
+    {
+        nodes = new MazeNode[(int)world.height][];
+        for (int verticalIndex = 0; verticalIndex < nodes.Length; verticalIndex += 1)
+        {
+            nodes[verticalIndex] = new MazeNode[(int)world.width];
+            /*for (int horizontalIndex = 0; horizontalIndex < nodes[verticalIndex].Length; horizontalIndex += 1)
+            {
+                nodes[verticalIndex][horizontalIndex] = CreateNode(horizontalIndex, verticalIndex);
+            }*/
+        }
+        MazeNode firstNode = GetOrCreateNode(
+            (int)Random.Range(world.xMin, world.xMax),
+            (int)Random.Range(world.yMin, world.yMax)
+        );
+        Carve(firstNode);
+        Traverse();
+    }
+
+    private void Carve(MazeNode node)
+    {
+        node.IsOpen = true;
+        carvedNodes.Add(node);
+        CreateRectSprite(node.Rect, Color.yellow);
+    }
+
+    private void CarveBetween(MazeNode dirNode, MazeNode node)
+    {
+        int dirX = (int)dirNode.Rect.x;
+        int dirY = (int)dirNode.Rect.y;
+        int targetX = (int)node.Rect.x;
+        int targetY = (int)node.Rect.y;
+
+        int newX = dirX;
+        int newY = dirY;
+        if (dirX < targetX) {
+            newX = dirX + 1;
+        }
+        if (dirX > targetX) {
+            newX = dirX - 1;
+        }
+        if (dirY < targetY) {
+            newY = dirY + 1;
+        }
+        if (dirY > targetY) {
+            newY = dirY - 1;
+        }
+        Debug.Log(dirNode.Rect);
+        Debug.Log(node.Rect);
+        Debug.Log(newX + " - " + newY);
+        MazeNode betweenNode = GetOrCreateNode(newX, newY);
+        CreateRectSprite(betweenNode.Rect, Color.magenta);
+
+        node.IsOpen = true;
+        carvedNodes.Add(node);
+        CreateRectSprite(node.Rect, Color.yellow);
+    }
+
+
+    private bool AttemptToCarve(MazeNode dirNode, MazeNode targetNode)
+    {
+        if (targetNode.IsOpen) {
+            return false;
+        }
+        List<MazeNode> neighbors = GetNeighbors(targetNode);
+        bool carvingIsPossible = neighbors
+            .Where(neighbor => !neighbor.Equals(dirNode))
+            .All(neighbor => !neighbor.IsOpen);
+
+        /*Debug.Log("\nneighbors: ");
+        foreach(MazeNode neighbor in neighbors) {
+            Debug.Log(string.Format("({2}, {3})Open: {0}, equals: {1}" ,
+                neighbor.IsOpen,
+                neighbor.Equals(dirNode),
+                neighbor.Rect.x,
+                neighbor.Rect.y
+            ));
+        }
+        Debug.Log("end neighbors \n");*/
+
+        /*Debug.Log(string.Format("Carving from {0}, {1} to {2}, {3} is: {4}",
+            dirNode.Rect.x,
+            dirNode.Rect.y,
+            targetNode.Rect.x,
+            targetNode.Rect.y,
+            carvingIsPossible ? "Possible" : "NOT POSSIBLE"
+        ));*/
+
+        if (carvingIsPossible)
+        {
+            Carve(targetNode);
+        }
+        return carvingIsPossible;
+    }
+
+    private void Traverse()
+    {
+        if (carvedNodes.Count < 1)
+        {
+            Debug.Log("No more!");
+            return;
+        }
+        MazeNode node = carvedNodes[Random.Range(0, carvedNodes.Count)];
+        List<MazeNode> neighbors = GetNeighbors(node);
+        //MazeNode neighbor = neighbors[Random.Range(0, neighbors.Count)];
+        //if (neighbor)
+        //Debug.Log(string.Format("Finding neighbors for {0}, {1}", node.Rect.x, node.Rect.y));
+        bool carveSuccesful = false;
+        List<MazeNode> cleanNeighbors = neighbors.FindAll(neighbor => !neighbor.IsOpen);
+//        Debug.Log(cleanNeighbors.Count);
+        if (cleanNeighbors.Count == 0) {
+            carvedNodes.Remove(node);
+        } else {
+            //Carve(cleanNeighbors[Random.Range(0, cleanNeighbors.Count)]);
+            CarveBetween(node, cleanNeighbors[Random.Range(0, cleanNeighbors.Count)]);
+        }
+        /*foreach (MazeNode neighbor in neighbors)
+        {
+            //Debug.Log(string.Format("Neighbor {0}, {1}", neighbor.Rect.x, neighbor.Rect.y));
+            if (AttemptToCarve(node, neighbor))
+            {
+                //Debug.Log(string.Format("Succesfully carved {0}, {1}", neighbor.Rect.x, neighbor.Rect.y));
+                carveSuccesful = true;
+                break;
+            }
+            //Random.Range()
+        }*/
+        /*if (!carveSuccesful)
+        {
+            carvedNodes.Remove(node);
+        }*/
+        //Traverse();
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Traverse();
+        }
+    }
+
+    private SpriteRenderer CreateRectSprite(Rect rect, Color color)
+    {
+        if (spritePrefab == null)
+        {
+            spritePrefab = Resources.Load<SpriteRenderer>("RoomSprite");
+        }
+        SpriteRenderer spriteRenderer = Instantiate(spritePrefab, Vector2.zero, Quaternion.identity);
+        spriteRenderer.transform.parent = transform;
+        spriteRenderer.transform.localScale = new Vector2(rect.width, rect.height);
+        spriteRenderer.transform.position = rect.position;
+        spriteRenderer.gameObject.SetActive(true);
+        spriteRenderer.color = color;
+        return spriteRenderer;
+    }
+    public MazeNode GetOrCreateNode(int x, int y)
+    {
+        MazeNode node = nodes[y][x];
+        if (node == null) {
+            node = new MazeNode(x, y);
+            nodes[y][x] = node;
+        }
+        return node;
+    }
+
+    List<MazeNode> GetNeighbors(MazeNode node)
+    {
+        List<MazeNode> neighbors = new List<MazeNode>();
+        /*for (int verticalIndex = -1; verticalIndex <= 1; verticalIndex += 1)
+        {
+            for (int horizontalIndex = -1; horizontalIndex <= 1; horizontalIndex += 1)
+            {*/
+        foreach (Vector2 pos in positions)
+        {
+            int x = (int)node.Rect.x + (int)pos.x;
+            int y = (int)node.Rect.y + (int)pos.y;
+            bool isWithinBounds = (y >= 0 && nodes.Length > y) && (x >= 0 && nodes[y].Length > x);
+            if (isWithinBounds)
+            {
+                MazeNode foundNode = GetOrCreateNode(x, y);
+                neighbors.Add(foundNode);
+            }
+        }
+        /*}
+    }*/
+        return neighbors;
+    }
+}
